aa93f76bc996cce7145100c3bef918930995f276
lysmac <calle@lysmask.se>
2022-11-30 17:29:19 +0100

    added mediaqueries, and startet to build design in html/css

--- main.js
+++ main.js
@@ -100,8 +100,7 @@ function addActiveCourses() {
       <div>
       ${value.courseName} <br>
       ${progressBarFunction(value.startDate, value.endDate)} %
-      <br>
-      ${value.endDate}
+      
       
       </div>`;
       } else {
